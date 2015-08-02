feec74150ca5264992a77864705ef35f2fce3222
gcsgpp <gcsgpp@gmail.com>
2015-08-02 02:16:53 -0300

    Revert "correções do algoritmo"
    This reverts commit 44d8612318c36c24247935b608517c5f06b2c40a.

--- famosos.py
+++ famosos.py
@@ -6,63 +6,48 @@ import urllib.request
 import urllib.error
 
 def limpezaNome(i):
-    i = str(i)
-    i = i.replace("'",""); '''Retirada de aspas simples dos nomes '''
+    i['nome'] = i['nome'].replace("'",""); '''Retirada de aspas simples dos nomes '''
 
     '''Retirada de conectivos dos nomes (exemplo: "do da de") '''
-    todosNomes = i.split(" ")
-    i = ""
+    todosNomes = i['nome'].split(" ")
+    i['nome'] = ""
     for j in todosNomes:
         if len(j) > 3 or j == todosNomes[0] or j == todosNomes[len(todosNomes) - 1]:
-            if len(i) == 0:
-                i = j
+            if len(i['nome']) == 0:
+                i['nome'] = j
             else:
-                i += " " + j
+                i['nome'] += " " + j
 
     ''' fim da retirada doss conectivos '''
 
-    return i
+    return i['nome']
 
 def crawlerFamosoRelacionados():
     listafamosos = model.buscarListaFamoso()
     for i in listafamosos:
-        famoso = {'id': i[0], 'nome': i[1].strip(), 'link': i[2], 'nascimento': i[3], 'idade': i[4], 'signo': i[5], 'relacionamento': i[6], 'conjuge': i[7]}
-        try:
-            html = urllib.request.urlopen("http://ego.globo.com" + famoso['link'])
-            soup = bs(html, "html.parser")
-            listaRelacionados = soup.find("ul", class_ = "famosos-relacionados")
+        famoso = {'id': i[0], 'nome': i[1].decode("utf-8"), 'link': i[2].decode("utf-8"), 'nascimento': i[3], 'idade': i[4], 'signo': i[5], 'relacionamento': i[6], 'conjuge': i[7]}
+        html = urllib.request.urlopen("http://ego.globo.com" + famoso['link'])
+        soup = bs(html, "html.parser")
+        listaRelacionados = soup.find("ul", class_ = "famosos-relacionados").find_all("a")
 
-            if listaRelacionados != None:
-                listaRelacionados = listaRelacionados.find_all("a")
+        print("Inserindo famosos relacionados de: " + str(famoso['nome']))
+        for j in listaRelacionados:
+            linkRelacionado = j.attrs['href']
+            nomeRelacionado = j.find("img").attrs['alt']
+            famosoRelacionado = {"idFamoso1": famoso['id'], "nome": nomeRelacionado, "link": linkRelacionado }
 
-                print(str(famoso['nome']))
-                for j in listaRelacionados:
-                    linkRelacionado = j.attrs['href']
-                    nomeRelacionado = j.find("img").attrs['alt']
-                    '''nomeRelacionado = limpezaNome(nomeRelacionado)'''
-                    famosoRelacionado = {"idFamoso1": famoso['id'], "nome": nomeRelacionado, "link": linkRelacionado }
-
-                    if model.verificarFamosoRelacionadoJaInserido(famosoRelacionado['idFamoso1'],famosoRelacionado['nome']) == False:
-                        if model.inserirFamosoRelacionado(famosoRelacionado) == True:
-                            print("  - Famoso: " + str(famosoRelacionado['nome']) + " inserido com sucesso.")
-                    else:
-                        print("  - Famoso: " + str(famosoRelacionado['nome']) + " já inserido.")
-        except urllib.error.HTTPError as e:
-            if e.code == 404:
-                print("Pagina do famoso nao encontrada")
-                model.gravarLog(idMensagens = 11, idFamoso = famoso['id'])
+            if model.verificarFamosoRelacionadoJaInserido(famosoRelacionado['idFamoso1'],famosoRelacionado['nome']) == False:
+                model.inserirFamosoRelacionado(famosoRelacionado)
+                print("  - Famoso: " + str(famosoRelacionado['nome']) + " inserido com sucesso.")
             else:
-                print(str(e) + " Famoso: " + str(famoso['nome']))
-
+                print("  - Famoso: " + str(famosoRelacionado['nome']) + " já inserido.")
 
 def crawlerListaFamosos():
     html = urllib.request.urlopen("http://ego.globo.com/famosos/")
     soup = bs(html, "html.parser")
     listahtmlfamosos = soup.find(id = 'main').find_all("a")
-    qtdAtualFamosos = 0
-    qtdTotalFamosos = len(listahtmlfamosos)
+    listafamosos = []
     for i in listahtmlfamosos:
-        qtdAtualFamosos += 1
         famoso = { 'link': i.get('href'), 'nome': i.get_text() }
 
         try:
@@ -90,8 +75,7 @@ def crawlerListaFamosos():
                     conjuge = conjuge.find("a")
                     if conjuge != None:
                         conjugeNome = conjuge.get_text()
-                        '''conjugeNome = limpezaNome(conjugeNome)'''
-
+                        conjugeNome = limpezaNome({"nome": conjugeNome})
                         conjuge = model.inserirConjuge(conjugeNome, famoso['nome'])
 
                 idade = detalheshtml.find("li", class_ = "aniversario")
@@ -110,30 +94,20 @@ def crawlerListaFamosos():
                     signo = signo.find("a").get_text()
                     signo = model.buscarSigno(signo, famoso['nome'])
 
-            '''famoso['nome'] = limpezaNome(famoso['nome'])'''
-            famoso.update({'nome': famoso['nome'].strip(), 'idade': idade, 'datanascimento': datanascimento, 'signo': signo, 'relacionamento': relacionamento, 'conjuge': conjuge })
 
-            model.inserirfamososdb(famoso,qtdAtualFamosos, qtdTotalFamosos)
+            famoso.update({'idade': limpezaNome(famoso), 'idade': idade, 'datanascimento': datanascimento, 'signo': signo, 'relacionamento': relacionamento, 'conjuge': conjuge })
+
+
+            model.inserirfamososdb(famoso)
+
+
         except urllib.error.HTTPError as err:
             if err.code == 404:
                 model.gravarLog(idMensagens = 11, nomeFamoso = famoso['nome'])
                 print("Página não encontrada. Gravado no log com sucesso! Famoso: " + famoso['nome'])
-
-                relacionamento = conjuge = idade = datanascimento = signo = 0
-
-                '''famoso['nome'] = limpezaNome(famoso['nome'])'''
-                famoso.update({'nome': famoso['nome'], 'idade': idade, 'datanascimento': datanascimento, 'signo': signo, 'relacionamento': relacionamento, 'conjuge': conjuge })
-                model.inserirfamososdb(famoso)
-
             else:
                 model.gravarLog(idMensagens = 12, nomeFamoso = famoso['nome'])
                 print("Erro inesperado ao obter detalhes do famoso. Gravado no log com sucesso! Famoso: " + famoso['nome'])
 
-crawlerListaFamosos()
-print("*****************************************************")
-print("*****************************************************")
-print("***********INICIO FAMOSO RELACIONADO*****************")
-print("*****************************************************")
-print("*****************************************************")
-
+'''crawlerListaFamosos()'''
 crawlerFamosoRelacionados()
--- noticias.py
+++ noticias.py
@@ -6,8 +6,6 @@ import urllib.request
 import urllib.error
 import model
 import time
-from fuzzywuzzy import fuzz
-from fuzzywuzzy import process
 
 def buscarnoticiasjson(pagina):
     tentarOutraPagina = True
@@ -46,7 +44,7 @@ def buscarnoticiasjson(pagina):
 
 def processarNoticias():
     noticias = {}
-    pagina = 479; ''' 496; ultima pagina: 5364'''
+    pagina = 1
     while noticias != False:
         noticias = buscarnoticiasjson(pagina)
         if noticias != False:
@@ -54,12 +52,10 @@ def processarNoticias():
             for i in noticias['noticias']:
                 html = urllib.request.urlopen(i['permalink'])
                 soup = bs(html, "html.parser")
+                i['titulo'] = i['titulo'].strip()
                 i['titulo'] = i['titulo'].replace("\n","")
                 i['titulo'] = i['titulo'].replace("\t","")
                 i['titulo'] = i['titulo'].replace("\r","")
-                i['titulo'] = i['titulo'].replace('"', "'")
-                i['titulo'] = i['titulo'].replace("\u266a","")
-                i['titulo'] = i['titulo'].strip()
 
                 if model.verificarNoticiaInserida(i['titulo'], i['permalink']) == False:
                     if i['tipo'] == "materia":
@@ -70,166 +66,17 @@ def processarNoticias():
                             temp = temp.replace("\n","")
                             temp = temp.replace("\t","")
                             temp = temp.replace("\r","")
-                            temp = temp.replace("\u266a","")
                             if len(temp) > 3:
                                 texto += temp
 
                         texto = texto.replace('"',"'")
-                        i['subtitulo'] = i['subtitulo'].replace('"', "'")
+                        i['titulo'] = i['titulo'].replace('"', "'")
+                        i['subtitulo']  = i['subtitulo'].replace('"', "'")
                     else:
                         '''Noticia do tipo Galeria que não possui texto'''
                         print("************ Noticia tipo GALERIA: " + i['permalink'] + " TIPO: " + str(i['tipo']))
                         texto = None
-                        i['subtitulo'] = i['subtitulo'].replace('"',"'")
                     noticia = { 'titulo': i['titulo'], 'tipo': i['tipo'], 'subtitulo': i['subtitulo'], 'link': i['permalink'], 'texto': texto, 'data': i['primeira_publicacao']}
                     model.cadastrarNoticia(noticia)
-        else:
-            tentarNovamente = False
-'''
-def extrairCitadosTexto():
-    listaFamosos = model.buscarListaFamoso()
-    listaNoticias = model.buscarListaNoticias()
-    f = open("arquivoTeste.txt", "w")
-    if listaNoticias != False and listaFamosos != False:
-        for i in listaNoticias[:5]:
-            f.writelines(str(i[1]) + "\n")
-            noticia = {'id': i[0], 'titulo': i[1], 'subtitulo': i[2], 'link': i[3], 'tipo': i[4], 'texto': i[5]}
-            print(str(noticia['titulo']))
+processarNoticias()
 
-            bestExtract = extrairCitadosTextoBestExtract(noticia['link'])
-
-            for j in bestExtract:
-                famoso = {'nome': j[0]}
-                ratioTotal = 0
-                mediaRatio = 0
-                nomeCompleto = famoso['nome']
-                todosNomes = famoso['nome'].split(" ")
-                textoTotal = noticia['titulo'] + " " + noticia['subtitulo'] + " " + noticia['texto']
-
-                ratioTotal += fuzz.token_set_ratio(nomeCompleto, textoTotal)
-                if len(todosNomes) > 1:
-                    for nome in todosNomes:
-                        ratioTotal += fuzz.token_set_ratio(nome, textoTotal)
-
-                if len(todosNomes) == 1:
-                    mediaRatio = ratioTotal
-                elif len(todosNomes) > 1:
-                    mediaRatio = ratioTotal / (len(todosNomes) + 1)
-
-                'comentario'print(str(nomeCompleto) + "," + str(mediaRatio))'comentario'
-                if mediaRatio > 20:
-                    f.writelines(str(nomeCompleto) + "," + str(mediaRatio)+"\n")
-
-                if mediaRatio > 85:
-                    'comentario'model.relacionarFamosoNoticia(famoso['nome'], noticia['id'])'comentario'
-                    'comentario'print(" - " + str(nomeCompleto))'comentario'
-        f.close()
-
-def extrairCitadosTextoBestExtract(link):
-    listaFamosos = model.buscarListaFamoso()
-    noticiaDB = model.buscarNoticia(link)
-    f = open("arquivoTeste2.txt", "w")
-    if noticiaDB != False and listaFamosos != False:
-        f.writelines(str(noticiaDB[1]) + "\n")
-        noticia = {'id': noticiaDB[0], 'titulo': noticiaDB[1], 'subtitulo': noticiaDB[2], 'link': noticiaDB[3], 'tipo': noticiaDB[4], 'texto': noticiaDB[5]}
-        print(str(noticia['titulo']))
-        famosos = []
-        for j in listaFamosos:
-            famosos.append(j[1])
-
-        textoTotal = noticia['titulo'] + " " + noticia['subtitulo'] + " " + noticia['texto']
-
-        result = process.extractBests(textoTotal, famosos,None,None,57,limit=10000)
-
-        for r in result:
-            f.writelines(str(r[0])+","+str(r[1])+"\n")
-
-        f.close()
-        return result
-'''
-
-def extrairCitadosTexto():
-    listaFamosos = model.buscarListaFamoso()
-    listaNoticias = model.buscarListaNoticias()
-    qtdNoticiaAtual =0
-    qtdTotalNoticias = len(listaNoticias)
-    if listaNoticias != False and listaFamosos != False:
-        for i in listaNoticias:
-            qtdNoticiaAtual += 1
-            noticia = {'id': i[0], 'titulo': i[1], 'subtitulo': i[2], 'link': i[3], 'tipo': i[4], 'texto': i[5]}
-            print(str(qtdNoticiaAtual) + "/" + str(qtdTotalNoticias) + " - " + str(noticia['titulo']))
-            for j in listaFamosos:
-                famoso = {'id': j[0], 'nome': j[1], 'link': j[2], 'dataNascimento': j[3], 'idade': j[4], 'signo': j[5], 'relacionamento': j[6], 'conjuge': j[7]}
-                ratioTotal = 0
-                mediaRatio = 0
-                nomeCompleto = famoso['nome']
-                todosNomes = famoso['nome'].split(" ")
-                textoTotal = noticia['titulo'] + " " + noticia['subtitulo'] + " " + noticia['texto']
-
-                ratioTotal += fuzz.token_set_ratio(nomeCompleto, textoTotal)
-
-                if len(todosNomes) == 1:
-                    mediaRatio = ratioTotal
-                elif len(todosNomes) > 1:
-                    for nome in todosNomes:
-                        ratioTotal += fuzz.token_set_ratio(nome, textoTotal)
-
-                    mediaRatio = ratioTotal / (len(todosNomes) + 1)
-
-                if mediaRatio > 85:
-                    if model.relacionarFamosoNoticia(famoso['nome'], noticia['id']) == False:
-                        print(" - " + str(nomeCompleto) + " ja relacionado com a noticia.")
-                    else:
-                        print(" - " + str(nomeCompleto))
-'''
-def extrairCitadosTextoProcess():
-    listaFamosos = model.buscarListaFamoso()
-    listaNoticias = model.buscarListaNoticias()
-    noticia =
-    f = open("arquivoTeste2.txt", "w")
-    if listaNoticias != False and listaFamosos != False:
-        for i in listaNoticias:
-            f.writelines(str(i[1]) + "\n")
-            noticia = {'id': i[0], 'titulo': i[1], 'subtitulo': i[2], 'link': i[3], 'tipo': i[4], 'texto': i[5]}
-            print(str(noticia['titulo']))
-            famosos = []
-            for j in listaFamosos:
-                famosos.append(j[1])
-
-            textoTotal = noticia['titulo'] + " " + noticia['subtitulo'] + " " + noticia['texto']
-
-            qtdProcessamento = 1
-            while qtdProcessamento < 3:
-
-                if qtdProcessamento == 1:
-                    listaNomes = famosos
-                else:
-                    listaNomesTemp = []
-                    for j in listaNomes:
-                        listaNomesTemp.append(j[0])
-                    listaNomes = listaNomesTemp
-                listaNomes = process.extractBests(textoTotal, listaNomes,None,None,57,limit=10000)
-                qtdProcessamento += 1
-
-            for r in listaNomes:
-                f.writelines(str(r[0])+","+str(r[1])+"\n")
-
-        f.close()
-'''
-
-
-tentarNovamente = True
-qtd = 0
-while tentarNovamente == True:
-    try:
-        processarNoticias()
-        qtd = 0
-    except Exception as e:
-        print(str(e))
-        print("Esperando 5 segundos.....")
-        time.sleep(5)
-        qtd += 1
-        if qtd > 5:
-            tentarNovamente = False
-'''extrairCitadosTexto()'''
-print("Fim da execucao")
